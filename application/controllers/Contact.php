392eea84ab55e41daf56d8d8cb8d11d67bc2288b
Hala <halaelyabouri@gmail.com>
2019-06-15 15:33:30 +0100

    update admin : contact complete

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -15,4 +15,54 @@ class Contact extends CI_Controller
         $data['nom'] = $this->profileInfo->get_info();
         $this->load->view('admin/contact',$data);
     }
+
+    function fetch_data()
+    {
+        $data = $this->Contact_model->make_query();
+        $array = array();
+        foreach($data as $row)
+        {
+            $array[] = $row;
+        }
+        $output = array(
+            'current'  => intval($_POST["current"]),
+            'rowCount'  => 10,
+            'total'   => intval($this->Contact_model->count_all_data()),
+            'rows'   => $array
+        );
+        echo json_encode($output);
+    }
+
+    function fetch_single_data()
+    {
+        if($this->input->post('id'))
+        {
+            $data = $this->Contact_model->fetch_single_data($this->input->post('id'));
+            foreach($data as $row)
+            {
+                $output['nom'] = $row['nom'];
+                $output['email'] = $row['email'];
+                $output['message'] = $row['message'];
+            }
+            echo json_encode($output);
+        }
+    }
+
+    function insert(){
+        $data = array(
+            'nom' => $this->input->post('nom'),
+            'email' => $this->input->post("email"),
+            'message' => $this->input->post("message"),
+        );
+        $this->Contact_model->insert($data);
+        redirect(base_url());
+    }
+    function delete_data()
+    {
+        if($this->input->post('id'))
+        {
+            $this->Contact_model->delete($this->input->post('id'));
+            echo 'Data Deleted';
+        }
+    }
 }
--- application/views/admin/contact.php
+++ application/views/admin/contact.php
@@ -5,7 +5,22 @@
         <?php require 'includes/nav.php'; ?>
 
         <div class="content">
-
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table id="contact_data" class="table table-striped table-bordered">
+                            <thead>
+                            <tr>
+                                <th data-column-id="nom">Nom</th>
+                                <th data-column-id="email">Email</th>
+                                <th data-column-id="message">Message</th>
+                                <th data-column-id="action" data-formatter="action" data-sortable="false">Action</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -13,3 +28,49 @@
 
 <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
 <script src="assets/js/light-bootstrap-dashboard.js"></script>
+
+<script type="text/javascript" language="javascript" >
+    $(document).ready(function(){
+
+        var contactTable = $('#contact_data').bootgrid({
+            ajax:true,
+            rowSelect: true,
+            post:function()
+            {
+                return{
+                    id:"b0df282a-0d67-40e5-8558-c9e93b7befed"
+                }
+            },
+            url:"<?php echo base_url(); ?>contact/fetch_data",
+            formatters:{
+                "action":function(column, row)
+                {
+                    return "<button type='button' class='btn btn-danger btn-xs delete' data-row-id='"+row.id+"' style='border:none;'><span class='glyphicon glyphicon-remove' style='color:#000'/></button>";
+                }
+            }
+        });
+
+        $(document).on("loaded.rs.jquery.bootgrid", function(){
+            contactTable.find('.delete').on('click', function(event){
+                if(confirm("Are you sure you want to delete this?"))
+                {
+                    var id = $(this).data('row-id');
+                    $.ajax({
+                        url:"<?php echo base_url(); ?>contact/delete_data",
+                        method:"POST",
+                        data:{id:id},
+                        success:function(data)
+                        {
+                            alert(data);
+                            $('#contact_data').bootgrid('reload');
+                        }
+                    });
+                }
+                else
+                {
+                    return false;
+                }
+            });
+        });
+    });
+</script>
